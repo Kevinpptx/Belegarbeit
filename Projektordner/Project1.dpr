9a27990db13cb7c55338e2073f75de1d35dcfe97
Kevinpptx <kb@kevin-home.de>
2024-04-27 10:40:30 +0200

    initial push

---
+++ Projektordner/Project1.dpr
@@ -0,0 +1,16 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  UMain in 'UMain.pas' {Form1},
+  UField in 'UField.pas',
+  UFigur in 'UFigur.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ Projektordner/UFigur.pas
@@ -0,0 +1,26 @@
+unit UFigur;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+
+type
+  TFigur = class
+  private
+
+    istWeiss : boolean;
+    legaleZuege : array[1..8, 1..8] of integer;
+    // hier war ich stehen geblieben
+
+  public
+
+  end;
+
+  // TODO: TShape Arrays auf TField ändern und dann Klassen für die Sachen
+
+implementation
+
+end.
+
